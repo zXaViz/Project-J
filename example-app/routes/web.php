733ca5e9b571367f314826a6f24a9fdb7c94411c
TunKiet <ttk.17092003@gmail.com>
2024-04-12 08:42:08 +0700

    Create By Kiet

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\CrudUserController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::get('dashboard', [CrudUserController::class, 'dashboard']);
 Route::get('create', [CrudUserController::class, 'createUser'])->name('user.createUser');
 Route::post('create', [CrudUserController::class, 'postUser'])->name('user.postUser');
 
